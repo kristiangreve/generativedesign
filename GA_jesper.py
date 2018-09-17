989e907ec4d3b02e00dabe4ace7c11fdf6a14654
jalkestrup <jalkestrup@gmail.com>
2018-09-17 15:39:15 +0200

    Jesper NSGA

---
+++ GA_jesper.py
@@ -0,0 +1,8 @@
+
+class individual:
+    def __init__(self,vector,rank,pareto_dominance,pareto_dominated):
+        self.vector = v
+        self.rank = r
+        self.pareto_dominance = dominates_these
+        self.pareto_dominated = dominated_by
+        
