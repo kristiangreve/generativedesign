ec988e8fd3e4caa2da61eacbb25eaaf455109154
kristiangreve <32651013+thebiggestmouseinthehouse@users.noreply.github.com>
2018-09-17 15:46:35 +0200

    committed

--- nsgaii.py
+++ nsgaii.py
@@ -27,7 +27,7 @@ from random import randint
 
 # class for design objects
 # scoring should be vector of length n_objective
-class solution(object):
+class solutionObject(object):
     """
     Object to contain solutions and their attributes.
     Attributes:
@@ -52,54 +52,61 @@ class solution(object):
 # cost functions
 
 # area to be minimized
-def cost_function_1(solution):
-    d_1,d_2,h=solution.parameters
+def cost_function_1(parameters):
+    d_1,d_2,h=parameters
     a_1 = h*np.pi*d_1
     a_2 = h*np.pi*d_2
     a_3 = np.pi*(d_1/2)**2 - np.pi*(d_2/2)**2
     return a_1+a_2+2*a_3
 
+
 # volume to be maximized
-def cost_function_2(solution):
-    d_1,d_2,h=solution.parameters
+def cost_function_2(parameters):
+    d_1,d_2,h=parameters
     v_1 = h*np.pi*(d_1/2)**2
     v_2 = h*np.pi*(d_2/2)**2
     return v_1 - v_2
 
-### initiate population
+
+
+### initiate population ###
 def init_population(size, n_dim):
     # create an empty population and fill it with random solutions
     pop = []
     for n in range(size):
         attributes = [randint(0,100) for i in range(n_dim)]
-        pop.append(solution(attributes))
+        pop.append(solutionObject(attributes))
     return pop
 
 
-### evaluate designs
 
-cost_functions=[cost_function_1,cost_function_2]
+
+### evaluate designs ###
+
+# assign score to each design by the use of cost functions
+def scoring(population):
+    for solution in population:
+        scoring = []
+        scoring.append(cost_function_1(solution.parameters))
+        scoring.append(cost_function_2(solution.parameters))
+        solution.scoring = scoring
 
 
 """
-def evaluate(population):
-    # assign score to each design
-    for design in population:
-        design.scoring = [
-"""
-
 
 def rank_designs(population):
     for function in cost_functions:
         for genome in population:
             genome.scoring
 
+"""
+### script
 
 population = init_population(10,3)
+scoring(population)
+
+
 
 for solution in population:
     print(solution.parameters)
-
-print(solution.parameters)
-print(solution.scoring)
-solution = solution([1,4,3])
+    print(solution.scoring)
