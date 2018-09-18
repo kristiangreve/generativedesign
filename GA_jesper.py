9fbb29e29566a786786e9cc58417bfd15fc6baf7
jalkestrup <jalkestrup@gmail.com>
2018-09-18 10:31:21 +0200

    Added init pop

--- GA_jesper.py
+++ GA_jesper.py
@@ -1,8 +1,16 @@
 
+import random
+
 class individual:
-    def __init__(self,vector,rank,pareto_dominance,pareto_dominated):
-        self.vector = v
-        self.rank = r
-        self.pareto_dominance = dominates_these
-        self.pareto_dominated = dominated_by
-        
+    def __init__(self,parameters,scores = [], rank = None,dominated_by = [], dominates_these = []):
+        self.parameters = parameters
+        self.rank = rank
+        self.scores = scores
+        self.dominated_by = dominated_by
+        self.dominates_these = dominates_these
+
+def init_population(size,dim):
+    population = []
+    for n in range(size):
+        population.append(individual([random.randrange(0,100) for i in range(dim)]))
+    return population
