fe7cf7932b225954303d4b182aefdac2bc0c36fb
jalkestrup <jalkestrup@gmail.com>
2018-10-25 14:59:37 +0200

    fixed replicate problem

--- GA/GA_core.py
+++ GA/GA_core.py
@@ -79,7 +79,7 @@ def dominance(population):
             #    population[j].dominates_these.append(population[i])
             #    population[i].dominated_count += 1
 
-def pareto(population):
+def pareto_score(population):
     pareto_counter = 1
     next_front = []
     cur_front = []
@@ -100,6 +100,11 @@ def pareto(population):
         next_front = []
         pareto_counter +=1
 
+    for obj in population:
+        if obj.pareto == None:
+            print(obj , 'does not have a pareto. # Dominated count/dominates: ', obj.dominated_count, len(obj.dominates_these))
+            print(obj.dominated_count)
+
 def hamming_distance(individual1, individual2):
     hamming = 0
     for index, dir in enumerate(individual1.dir_list):
@@ -117,9 +122,14 @@ def dir_score(pareto_front):
                         hamming_list.append(hamming_distance(individual,comparison))
                         if hamming_list[-1] > max_value:
                             max_value = hamming_list[-1]
-
-
                 hamming_list = sorted(hamming_list)
+
+                if len(hamming_list)<2:
+                    print('Error about to occur!')
+                    print('Len of pareto: ', len(pareto_front))
+                    print('Objects in pareto: ')
+                    for obj in pareto_front:
+                        print(obj)
 
                 individual.dir_score = (hamming_list[0]+hamming_list[1]) / 2
                 #print('Hamming list: ', hamming_list)
@@ -127,8 +137,11 @@ def dir_score(pareto_front):
                 individual.dir_score = 1
         #print('Max: ', max_value)
         if len(pareto_front)>2:
+
             for individual in pareto_front:
-                individual.dir_score =  individual.dir_score / max_value
+
+                if max_value != 0:
+                    individual.dir_score =  individual.dir_score / max_value
                 #print('Dir score: ', individual.dir_score)
 
     #for index, individual in enumerate(pareto_front): #more efficient but requires storing relations
@@ -144,8 +157,11 @@ def dir_crowding(population):
     for i in population:
         pareto_dict[i.pareto].append(i)
     for pareto_list in pareto_dict.values():
-        #print('Pareto front', pareto_list[0].pareto, 'with size: ', len(pareto_list))
-        #print('...and adjacency score: ', pareto_list[0].adjacency_score)
+        #for index,object in enumerate(pareto_list):
+        #    if index+1 < len(pareto_list):
+        #        if object == pareto_list[index+1]:
+        #            print('Input object similar!')
+        #            print(object)
         dir_score(pareto_list)
 
 def comparison(obj1,obj2): # Compares 2 individuals on pareto front, followed by crowding
@@ -154,7 +170,7 @@ def comparison(obj1,obj2): # Compares 2 individuals on pareto front, followed by
             return obj1
         else: #if equal, return nr 2 object parsed..
             return obj2
-    elif obj1.pareto > obj2.pareto:
+    elif obj1.pareto < obj2.pareto:
         return obj1
     else:
         return obj2
@@ -176,6 +192,9 @@ def crossover(obj1,obj2):
     child1 = individual(definition, room_def,(obj1.split_list[:(mid-1)]+obj2.split_list[(mid-1):]),(obj1.dir_list[:mid]+obj2.dir_list[mid:]),(child1_p1+child1_p2),min_opening)
     child2 = individual(definition, room_def,(obj2.split_list[:(mid-1)]+obj1.split_list[(mid-1):]),(obj2.dir_list[:mid]+obj1.dir_list[mid:]),(child2_p1+child2_p2),min_opening)
 
+    if (child1 or child2) == (obj1 or obj2): #troubleshooting
+        print('Child equal to parent!')
+        print(child1,child2,obj1,obj2)
     return child1,child2
 
 def breeding(population):
@@ -184,57 +203,97 @@ def breeding(population):
         parent1 = binary_tournament(population)
         parent2 = binary_tournament(population)
 
-        if parent1 != parent2:
-            child1,child2 = crossover(parent1,parent2) #Crossing dir lists
+        if parent1 != parent2: #to avoid breeding the same parent
+            child1,child2 = crossover(parent1,parent2) #
             children.append(child1)
             children.append(child2)
     return children
 
 def selection(pop_size, population):
     pareto_dict = defaultdict(list) #creates a dict for all pop and arranges according to pareto front
+    worst_pareto = population[0].pareto
     for i in population:
         pareto_dict[i.pareto].append(i)
-    new_gen = []
+        if i.pareto > worst_pareto:
+            worst_pareto = i.pareto
 
-    for pareto_counter in range(1,len(population)):
-        if pareto_counter == 1:
-            print('Pareto 1, adjacency score: ', pareto_dict[pareto_counter][0].adjacency_score)
+
+    new_gen = []
+    for pareto_counter in range(1,worst_pareto):
+        #if pareto_counter == 1: #to see if adjacancy gets better in time
+            #print('Pareto 1, adjacency score: ', pareto_dict[pareto_counter][0].adjacency_score)
         if (len(new_gen)+len(pareto_dict[pareto_counter])) < pop_size:
             for obj in pareto_dict[pareto_counter]:
                 new_gen.append(obj)
         else:
-            sorted_pareto = sorted(pareto_dict[pareto_counter], key=lambda x: x.dir_score, reverse=False)
+            sorted_pareto = sorted(pareto_dict[pareto_counter], key=lambda x: x.dir_score, reverse=True)
             for obj in sorted_pareto:
-                while len(new_gen) < pop_size:
+                if len(new_gen) < pop_size:
                     new_gen.append(obj)
-            break
+            #break
     return new_gen
 
 def generate(pop_size, generations):
     Pt = init_population(pop_size);
     evaluate_pop(Pt)
     dominance(Pt)
-    pareto(Pt)
+    pareto_score(Pt)
     dir_crowding(Pt)
+
+    print('Generation 0')
+    similar_counter = 0
+    for index,obj in enumerate(Pt):
+        if index+1 < len(Pt):
+            if obj == Pt[index+1]:
+                print('Similar Pt:')
+                print(obj)
+                print(Pt[index+1])
+    print('# of Similar objects: ', similar_counter)
 
     gen_counter = 1
     while gen_counter <= generations:
+        print('Generation: ', gen_counter)
         Qt = breeding(Pt)
-
         evaluate_pop(Qt)
+        for child in Qt:
+            if child in Pt:
+                print('Child equal to parent pop!')
         Rt = Pt + Qt
         dominance(Rt)
-        pareto(Rt)
-        print('Rt: ')
-        for obj in Rt:
-            print('obj :', obj)
+        pareto_score(Rt)
+        #print('Rt: ')
+        #for obj in Rt:
+        #    print('obj :', obj)
         dir_crowding(Rt)
+
+        print('Before selection: ')
+        similar2_counter = 0
+        for index,obj in enumerate(Rt):
+            if index+1 < len(Rt):
+                if obj == Rt[index+1]:
+                    if similar2_counter == 0:
+                        similar2_counter = 2
+                    else:
+                        similar2_counter +=1
+        print('# of Similar objects, before selection: ', similar2_counter)
+
+
         Pt = selection(pop_size,Rt)
+
+        similar_counter = 0
+        for index,obj in enumerate(Pt):
+            if index+1 < len(Pt):
+                if obj == Pt[index+1]:
+                    if similar_counter == 0:
+                        similar_counter = 2
+                    else:
+                        similar_counter +=1
+        print('# of Similar objects, after selection: ', similar_counter)
 
         gen_counter += 1
 
 
-generate(20,4)
+generate(40,8)
 
 """
 print("\nINPUTS:")
